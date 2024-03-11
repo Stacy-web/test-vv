f14adda8278a9ca2131eebc24c45dac610af37e3
Stacy-web <kisma6@mail.ru>
2024-03-11 17:49:49 +0300

    upload

---
+++ main.js
@@ -0,0 +1,54 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
+function clearAccordionItem(item) {
+    item.classList.remove('accordion__item_active');
+    linkText.removeAttribute('style');
+}
+
+const accordionItems = document.querySelectorAll('.accordion__item');
+
+accordionItems.forEach(item => {
+    const link = item.querySelector('.accordion__link');
+
+    link.addEventListener('click', e => {
+        e.preventDefault();
+
+        const linkText = item.querySelector('.accordion__text');
+
+        if (item.classList.contains('accordion__item_active')) {
+            item.classList.remove('accordion__item_active');
+            linkText.removeAttribute('style');
+        } else {
+            const linkTextScrollHeight = linkText.scrollHeight;
+
+            accordionItems.forEach(sibling => {
+                if (
+                    sibling.classList.contains('accordion__item_active')
+                ) {
+                    sibling.classList.remove('accordion__item_active');
+                    const linkText = sibling.querySelector('.accordion__text');
+
+                    linkText.removeAttribute('style');
+                }
+            });
+
+            linkText.style.maxHeight = `${linkTextScrollHeight}px`;
+            item.classList.add('accordion__item_active');
+        }
+    })
+});
